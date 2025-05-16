9b4b99658511f531f3accd6f5700618f5adc3c81
Hanu <hanubabbar321@gmail.com>
2025-05-16 17:48:14 +0530

    m

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -10,20 +10,7 @@ router.get('/', async (req, res) => {
     const products = await Product.find();
     res.json(products);
 });
- 
-// Add a new product (Admin Only)
-router.post('/', async (req, res) => {
-    try {
-        const { name, price, description, category, stock, image } = req.body;
 
-        const newProduct = new Product({ name, price, description, category, stock, image });
-        await newProduct.save();
-
-        res.status(201).json({ msg: "Product added successfully" });
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
-    }
-});
 
 // Temporarily bypass verifyToken for debugging
 router.get('/products', productController.getProducts);
